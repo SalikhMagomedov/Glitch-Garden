1581f149ccc09ad6ee51ac06b4582204233a8c11
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-07 12:03:52 +0300

    Make A Splash Screen

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private float timeToWait = 4f;
+
+    private int currentSceneIndex;
+
+    private IEnumerator Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            yield return new WaitForSeconds(timeToWait);
+            LoadNextScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
