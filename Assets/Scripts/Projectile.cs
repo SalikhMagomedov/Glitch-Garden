0dbc16510d8dd7a71006966f7253aacddc9f0c01
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-09 13:45:34 +0300

    Make Projectile Move

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed = 10f;
+
+    private void Update() => transform.Translate(Vector2.right * speed * Time.deltaTime);
+}
