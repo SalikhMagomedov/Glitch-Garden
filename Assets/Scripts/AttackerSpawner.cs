1d62fd2546eb44677d74e22c0683e75e136acf85
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-09 12:57:24 +0300

    Spawn Attackers Using Coroutine

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] private Attacker attacker;
+    [SerializeField] private float minSpawnDelay = 1f;
+    [SerializeField] private float maxSpawnDelay = 5f;
+
+    private bool spawn = true;
+
+    private IEnumerator Start()
+    {
+        while (spawn)
+        {
+            yield return new WaitForSeconds(UnityEngine.Random.Range(minSpawnDelay, maxSpawnDelay));
+            Spawn();
+        }
+    }
+
+    private void Spawn() => Instantiate(attacker, transform.position, Quaternion.identity);
+}
