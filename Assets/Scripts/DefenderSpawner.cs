9ed36bcd436d0d7575ea0c5bd61dfa616142cb14
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-10 21:09:05 +0300

    Click To Spawn Defenders

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject defenderPrefab;
+
+    private void OnMouseDown() => SpawnDefender();
+
+    private void SpawnDefender()
+    {
+        var newDefender = Instantiate(defenderPrefab, transform.position, Quaternion.identity);
+    }
+}
