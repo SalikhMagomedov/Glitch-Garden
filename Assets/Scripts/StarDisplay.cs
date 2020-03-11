25be14e2a3521db0b2a05ed4834d08488cfe5deb
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-11 20:44:26 +0300

    Start Our Resource System

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class StarDisplay : MonoBehaviour
+{
+    [SerializeField] private int stars = 100;
+
+    private TextMeshProUGUI starText;
+
+    private void Awake() => starText = GetComponent<TextMeshProUGUI>();
+
+    private void Start() => UpdateDisplay();
+
+    private void UpdateDisplay() => starText.text = stars.ToString();
+
+    public void AddStars(int amount)
+    {
+        stars += amount;
+        UpdateDisplay();
+    }
+
+    public void SpendStars(int amount)
+    {
+        if (stars < amount) { return; }
+
+        stars -= amount;
+        UpdateDisplay();
+    }
+}
