5f28ca5290332eee6da357325e3fc6177b7a4149
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-11 20:12:03 +0300

    Create Defender Selection UI

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DefenderButton : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake() => spriteRenderer = GetComponent<SpriteRenderer>();
+
+    private void OnMouseDown()
+    {
+        var buttons = FindObjectsOfType<DefenderButton>();
+        foreach (var button in buttons)
+        {
+            button.GetComponent<SpriteRenderer>().color = new Color32(25, 25, 25, 255);
+        }
+        spriteRenderer.color = Color.white;
+    }
+}
