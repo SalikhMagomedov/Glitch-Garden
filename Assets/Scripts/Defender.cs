131496627595ef119f889a7e1975f66952ed9710
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-12 20:55:07 +0300

    Add Resources As Anim Event

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -3,4 +3,6 @@
 public class Defender : MonoBehaviour
 {
     [SerializeField] private int starCost = 100;
+
+    public void AddStars(int amount) => FindObjectOfType<StarDisplay>().AddStars(amount);
 }
