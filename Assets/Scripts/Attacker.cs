3570e9a95365ca3400daff90bbfd04c8db32f11c
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-15 19:57:28 +0300

    Make Our Attackers Attack

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -3,8 +3,18 @@
 public class Attacker : MonoBehaviour
 {  
     private float currentSpeed = 1f;
+    private GameObject currentTarget;
+    private Animator animator;
+
+    private void Awake() => animator = GetComponent<Animator>();
 
     private void Update() => transform.Translate(Vector2.left * currentSpeed * Time.deltaTime);
 
     public void SetMovementSpeed(float speed) => currentSpeed = speed;
+
+    public void Attack(GameObject target)
+    {
+        animator.SetBool("IsAttacking", true);
+        currentTarget = target;
+    }
 }
