c9de5cffd38148a64710d6650bd0954b4b5b2aa0
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-16 20:42:56 +0300

    Fantastic Mr Fox

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private Attacker attacker;
+
+    private void Awake() => attacker = GetComponent<Attacker>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+
+        if (otherObject.GetComponent<Gravestone>() != null)
+        {
+            gameObject.GetComponent<Animator>().SetTrigger("JumpTrigger");
+        }
+
+        else if (otherObject.GetComponent<Defender>() != null)
+        {
+            attacker.Attack(otherObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Gravestone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Gravestone : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        var attacker = collision.GetComponent<Attacker>();
+        if (attacker)
+        {
+
+        }
+    }
+}
