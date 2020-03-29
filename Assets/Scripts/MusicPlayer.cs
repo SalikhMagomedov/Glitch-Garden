e0ca75f89e26a3d8006d3bae273e2ece51cf461a
Salikh Magomedov <sm.saarus@gmail.com>
2020-03-29 20:04:04 +0300

    Set & Save Music Volume

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.volume = PlayerPrefsController.MasterVolume;
+    }
+
+    public void SetVolume(float volume) => audioSource.volume = volume;
+}
---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private float defaultVolume = .8f;
+
+    private MusicPlayer musicPlayer;
+
+    private void Awake()
+    {
+        musicPlayer = FindObjectOfType<MusicPlayer>();
+    }
+
+    private void Start()
+    {
+        volumeSlider.value = PlayerPrefsController.MasterVolume;
+    }
+
+    private void Update()
+    {
+        if (musicPlayer)
+        {
+            musicPlayer.SetVolume(volumeSlider.value);
+        }
+        else
+        {
+            Debug.LogWarning("No music player found... did you start from splash screen?");
+        }
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.MasterVolume = volumeSlider.value;
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+
+    public void SetDefaults()
+    {
+        volumeSlider.value = defaultVolume;
+    }
+}
